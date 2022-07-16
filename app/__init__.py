592c50d6dc81dce4fd0493eebbc3b311848de0c7
Marcus Pereira <marcus@negros.dev>
2022-07-16 10:55:51 -0300

    Configurando banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from app import routes
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.sqlite'
+    db.init_app(app)
 
     routes.init_app(app)
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,19 @@
 from pytest import fixture
 from splinter import Browser
 
-from app import create_app
+from app import create_app, db
 
 @fixture
 def browser():
     app = create_app()
-    browser = Browser('flask', app=app)
-    return browser
+    app.testing = True
+    app_context = app.test_request_context()
+    app_context.push()
+
+    with app.test_client():
+        db.create_all()
+
+        yield Browser('flask', app=app)
+
+        db.session.remove()
+        db.drop_all()
