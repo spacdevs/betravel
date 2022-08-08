d31f13a742d94e5db69ea96033b41dda0d826b88
Marcus Pereira <marcus@negros.dev>
2022-08-08 10:10:13 -0300

    Set SigninController

---
+++ app/controllers/signin_controller.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, flash, redirect, render_template, url_for
+from flask_login import login_user
+from werkzeug.security import check_password_hash
+
+from app.forms import SignInForm
+from app.models import User
+
+
+sign_in = Blueprint("signin", __name__)
+
+class SigninController:
+    def new(self):
+        form = SignInForm()
+        return render_template("sessions/sign_in/new.jinja", form=form)
+
+    def create(self):
+        form = SignInForm()
+
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+
+            if not user or not check_password_hash(user.password, form.password.data):
+                flash("Usúario ou senha estão incorretas")
+                return redirect(url_for(".new"))
+
+            login_user(user)
+            flash(f"Bem-vindo de volta, {user.name}")
+            return redirect(url_for("home.index"))
+
+        return render_template("sessions/sign_in/new.jinja", form=form)
--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,5 @@ from mvc_flask import Router
 Router.get("/",             "home#index")
 Router.all("posts",         only="show new create")
 Router.all("categories",    only="new create")
-Router.all("signup",       only="new create")
-
-from app.blueprints.sessions.sign_in_blueprint import sign_in
-
-def init_app(app):
-    app.register_blueprint(sign_in)
+Router.all("signup",        only="new create")
+Router.all("signin",        only="new create")
