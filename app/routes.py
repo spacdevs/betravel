6cc3637734e893e2d6cf38f26243b3d9d7f139f5
Marcus Pereira <marcus@negros.dev>
2022-08-08 10:06:45 -0300

    Set SignupController

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,12 @@
 from mvc_flask import Router
 
 
-Router.get("/", "home#index")
-Router.all("posts", only="show new create")
-Router.all("categories", only="new create")
+Router.get("/",             "home#index")
+Router.all("posts",         only="show new create")
+Router.all("categories",    only="new create")
+Router.all("signup",       only="new create")
 
-from app.blueprints.sessions.sign_up_blueprint import sign_up
 from app.blueprints.sessions.sign_in_blueprint import sign_in
 
 def init_app(app):
     app.register_blueprint(sign_in)
-    app.register_blueprint(sign_up)
