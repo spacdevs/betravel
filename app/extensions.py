b0b14ec449ce1fd72e9d0835ea6382cc48d5a671
Marcus Pereira <marcus@negros.dev>
2022-08-08 09:57:26 -0300

    Install mvc-flask

--- app/extensions.py
+++ app/extensions.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_seeder import FlaskSeeder
+from mvc_flask import FlaskMVC
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -12,6 +13,7 @@ def init_app(app):
     db.init_app(app)
     FlaskSeeder(app, db)
     Migrate(app, db)
+    FlaskMVC(app)
 
     @app.shell_context_processor
     def _():
