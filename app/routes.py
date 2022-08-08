3177873ff4ac0b9da106e0e532e1c1a36768df74
Marcus Pereira <marcus@negros.dev>
2022-08-08 10:01:01 -0300

    Set PostsController

--- app/routes.py
+++ app/routes.py
@@ -2,15 +2,14 @@ from mvc_flask import Router
 
 
 Router.get("/", "home#index")
+Router.all("posts", only="show new create")
 
-from app.blueprints.posts_blueprint import posts
 from app.blueprints.categories_blueprint import categories
 from app.blueprints.sessions.sign_up_blueprint import sign_up
 from app.blueprints.sessions.sign_in_blueprint import sign_in
 
 
 def init_app(app):
-    app.register_blueprint(posts)
     app.register_blueprint(sign_in)
     app.register_blueprint(sign_up)
     app.register_blueprint(categories)
